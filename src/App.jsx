cf7b83a2f0912af77c433595caaa30efc7f447c6
GomezDario <dariogomez.g00@gmail.com>
2024-10-04 08:58:27 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
